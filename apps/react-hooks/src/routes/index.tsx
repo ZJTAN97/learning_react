73ab4cfae17ccdee986ee2fa4e08fb38befa6035
ZJ <zhijietan97@gmail.com>
2024-07-01 23:15:33 +0800

    chore: update styling for react hooks guide

--- apps/react-hooks/src/routes/index.tsx
+++ apps/react-hooks/src/routes/index.tsx
@@ -1,21 +1,74 @@
-import { Button, Code, List, Stack, Text, Title } from '@mantine/core';
+import { Button, Card, Code, List, Stack, Text, Title } from '@mantine/core';
 import { createFileRoute, Link } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: () => (
-    <Stack px="md">
-      <Title mb="md">Welcome to React Hooks</Title>
-      <Text fz="h2">useState Hooks</Text>
-      <List size="md">
-        <List.Item>
-          <Link
-            to="/use-state-hook/dont-over-usestate"
-            style={{ textDecoration: 'none' }}
-          >
-            Do not overuse <Code>useState</Code>
-          </Link>
-        </List.Item>
-      </List>
+    <Stack px="md" py="sm" gap="xl">
+      <Title>Welcome to React Hooks</Title>
+
+      <Card withBorder radius="md">
+        <Text size="xl" mb="sm">
+          The useState Hook
+        </Text>
+        <List size="md" ml="md">
+          <List.Item>
+            <Link
+              to="/use-state-hook/dont-over-usestate"
+              style={{ textDecoration: 'none' }}
+            >
+              Do not overuse useState hook
+            </Link>
+          </List.Item>
+          <List.Item>
+            <Link
+              to="/use-state-hook/dont-over-usestate"
+              style={{ textDecoration: 'none' }}
+            >
+              Do not overuse useState hook
+            </Link>
+          </List.Item>
+          <List.Item>
+            <Link
+              to="/use-state-hook/dont-over-usestate"
+              style={{ textDecoration: 'none' }}
+            >
+              Do not overuse useState hook
+            </Link>
+          </List.Item>
+        </List>
+      </Card>
+
+      <Card withBorder radius="md">
+        <Text size="xl" mb="sm">
+          The useCallback Hook
+        </Text>
+        <List size="md" ml="md">
+          <List.Item>
+            <Link
+              to="/use-state-hook/dont-over-usestate"
+              style={{ textDecoration: 'none' }}
+            >
+              Placeholder for link and article to useCallback
+            </Link>
+          </List.Item>
+          <List.Item>
+            <Link
+              to="/use-state-hook/dont-over-usestate"
+              style={{ textDecoration: 'none' }}
+            >
+              Placeholder for link and article to useCallback
+            </Link>
+          </List.Item>
+          <List.Item>
+            <Link
+              to="/use-state-hook/dont-over-usestate"
+              style={{ textDecoration: 'none' }}
+            >
+              Placeholder for link and article to useCallback
+            </Link>
+          </List.Item>
+        </List>
+      </Card>
     </Stack>
   ),
 });
